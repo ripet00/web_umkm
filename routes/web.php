3555f3c585788aaff04f035a4fefb6ae072cfaa4
Rifat <rifat.khaidir@gmail.com>
2025-09-24 06:07:48 +0800

    feat : add route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SellerController;
+use App\Http\Controllers\BuyerController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('sellers', SellerController::class);
+Route::resource('buyers', BuyerController::class);
