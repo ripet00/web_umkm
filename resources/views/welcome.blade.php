3d2bafe1d2b4293d7269bad4c53d7d9ca55ca684
EkaPutri <adifa.note2@gmail.com>
2025-09-25 22:52:17 +0800

    modified title to MarketChain

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'MarketChain') }}</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="antialiased bg-gray-100 dark:bg-gray-900">
