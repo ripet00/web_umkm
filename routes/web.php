2c4fc6b98765656fb652f567314e2a1270c935b6
Rifat <rifat.khaidir@gmail.com>
2025-09-26 10:08:54 +0800

    feat : fix route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\SellerRegisterController;
 use App\Http\Controllers\SellerLoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
@@ -19,6 +20,13 @@ Route::get('/home', [HomeController::class, 'index'])->name('home'); // Halaman 
 Route::get('login', function() { return view('auth.login'); })->middleware('guest')->name('login');
 // Halaman form login untuk User
 Route::get('user/login', function() { return view('auth.user-login'); })->middleware('guest:web')->name('user.login');
+// Halaman form register untuk User
+Route::get('user/register', function() { return view('auth.user-register'); })->middleware('guest:web')->name('user.register');
+
+// Halaman form login untuk User
+Route::get('seller/login', function() { return view('auth.seller-login'); })->middleware('guest:web')->name('seller.login');
+// Halaman form register untuk User
+Route::get('seller/register', function() { return view('auth.seller-register'); })->middleware('guest:web')->name('user.register');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -34,15 +42,15 @@ Route::middleware('auth:web')->group(function () { // Spesifik untuk guard 'web'
 Route::prefix('seller')->name('seller.')->group(function () {
     Route::get('login', [SellerLoginController::class, 'create'])->middleware('guest:seller')->name('login');
     Route::post('login', [SellerLoginController::class, 'store'])->middleware('guest:seller');
-    Route::post('logout', [SellerLoginController::class, 'destroy'])->middleware('auth:seller')->name('logout');
-});
-
+    Route::get('register', [SellerRegisterController::class, 'create'])->middleware('guest:seller')->name('register');
+    Route::post('register', [SellerRegisterController::class, 'store'])->middleware('guest:seller');
+    
 // --- Rute Khusus Seller (Dashboard & Manajemen Produk) ---
-Route::prefix('seller')->name('seller.')->middleware('auth:seller')->group(function () {
-    Route::get('/dashboard', function () {
-        return "Welcome to Seller Dashboard"; // Ganti dengan view dashboard seller Anda
-    })->name('dashboard');
-    Route::resource('products', ProductController::class);
+    Route::middleware('auth:seller')->group(function () {
+        Route::get('/dashboard', function () { return "Welcome to Seller Dashboard"; })->name('dashboard');
+        Route::post('logout', [SellerLoginController::class, 'destroy'])->name('logout');
+        Route::resource('products', ProductController::class);
+    });
 });
 
 require __DIR__.'/auth.php';
